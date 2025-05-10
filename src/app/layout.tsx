dba91c59a276f5c1fbced38b2e3f13078f6641a3
ahmad alhraki <ahmadhraki752@gmail.com>
2025-05-10 04:08:55 +0300

    m

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,23 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${neuton.className} antialiased select-none duration-300`}>
+      <head>
+        {/* Favicon for the browser */}
+        <link rel="icon" href="/favicon.ico" />
+
+        {/* Apple touch icon for home screen */}
+        <link rel="apple-touch-icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="120x120" href="/favicon.ico" />
+
+        {/* For Android Chrome */}
+        <meta name="mobile-web-app-capable" content="yes" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/favicon.ico" />
+      </head>
+      <body
+        className={`${neuton.className} antialiased select-none duration-300`}
+      >
         <Navigation />
         <main>{children}</main>
         <NetflixFooter />
