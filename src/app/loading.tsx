bb36af3d03b0b331915faed066eab722c6af1c9e
ahmad alhraki <ahmadhraki752@gmail.com>
2025-05-13 16:11:53 +0300

    m

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,7 +3,7 @@ import LoadingSpinner from "@/components/UI/LoadingSpinner";
 // app/loading.tsx
 export default function Loading() {
   return (
-    <div className="flex items-center justify-center h-screen mt-[60PX]">
+    <div className="flex items-center justify-center h-[calc(100vh-60px)]">
       <LoadingSpinner />
     </div>
   );
